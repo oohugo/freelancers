a84372083947672ecf783aed68800d965e2430a5
Hugo Araujo <hugo17@protonmail.com>
2021-10-08 21:25:18 -0300

    Add model worker for log in of freelancer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :employers
+  devise_for :workers, path: 'workers'
+  devise_for :employers, path: 'employers'
   root to: 'home#index'
   resources :projects, only: %i[create new]
 end
---
+++ spec/system/worker_log_in_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Worker log in' do
+  it 'successfully' do
+    joao = Worker.create!(email: 'joao@email.com', password: '123456')
+
+    visit root_path
+    expect(page).not_to have_content(joao.email)
+    click_on 'Entrar como freelancer'
+    fill_in 'Email', with: joao.email
+    fill_in 'Senha', with: joao.password
+    click_on 'Log in'
+
+    expect(page).to have_content('joao@email.com')
+    expect(page).to have_content('Projetos disponiveis')
+    expect(page).not_to have_content('Entrar como worker')
+  end
+
+  it 'worker logout' do
+    joao = Worker.create!(email: 'joao@email.com', password: '123456')
+
+    login_as joao, scope: :worker
+    visit root_path
+    click_on 'Logout'
+
+    expect(page).to have_content('Entrar como freelancer')
+    expect(page).to have_current_path(root_path)
+    expect(page).not_to have_content(joao.email)
+  end
+end
