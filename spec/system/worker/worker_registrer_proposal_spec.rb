d886df7a8d9a72a2a9d4c18335909bb2ba6f1454
Hugo Araujo <hugo17@protonmail.com>
2021-10-16 01:54:07 -0300

    Change folders for spec

--- spec/system/worker/worker_registrer_proposal_spec.rb
+++ spec/system/worker/worker_registrer_proposal_spec.rb
@@ -23,4 +23,16 @@ describe 'Worker register proposal' do
     expect(page).to have_content('Cancelar proposta')
     expect(page).to have_content('Site para contratar freelancers')
   end
+  pending 'in a suspend project' 
+    # worker = Worker.create!(email: 'email@email.com', password: '123456')
+    # employer = Employer.create!(email: 'employer@email.com', password: '123456')
+    # Project.create!(title: 'Site de freelancer', description: 'Site para contratar freelancers',
+    #                 max_per_hour: 10.0, deadline: 5.days.from_now, place: 'Remoto',
+    #                 employer: employer, status: :suspend)
+    # login_as worker, scope: :werker
+    # visit root_path
+    # click_on 'Site de freelancer'
+
+  pending 'in finished a project'
+  pending 'canceled a proposal'
 end
